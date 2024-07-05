e93a3dd5a23b21d787c589606017480b9ca4db0b
emred <femredemir699@gmail.com>
2024-07-05 22:43:17 +0300

    odev 10

--- src/main/java/DiziArama2_3/Main.java
+++ src/main/java/DiziArama2_3/Main.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] dizi = {1, 2, 3, 4, 5};
+        int[] dizi = {55, 28, 45, 41, 33, 3, 9};
         int aranan = 3;
         System.out.println(arama(dizi,aranan));
-
+        int aranan2 = 66;
+        System.out.println(arama(dizi,aranan2));
     }
     public static int arama(int[] dizi, int aranan){
         boolean bulunan = Arrays.stream(dizi).anyMatch(n -> n == aranan);
